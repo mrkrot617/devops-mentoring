7ff3ccc471da150e4ccd878658c77bedaa9e059b
Ivan Novitsky <mrkrot617@gmail.com>
2022-01-25 16:26:09 +0300

    Add internet gateway and route table

--- terraform/01-jenkins-server/main.tf
+++ terraform/01-jenkins-server/main.tf
@@ -11,10 +11,28 @@ resource "aws_vpc" "project_vpc" {
   cidr_block = "10.10.10.0/24"
 }
 
+resource "aws_internet_gateway" "project_internet_gateway" {
+  vpc_id =  aws_vpc.project_vpc.id
+}
+
+resource "aws_route_table" "public_RT" {
+  vpc_id =  aws_vpc.project_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.project_internet_gateway.id
+  }
+}
+
 resource "aws_subnet" "project_subnet" {
   vpc_id            = aws_vpc.project_vpc.id
   cidr_block        = "10.10.10.0/28"
   availability_zone = "${local.aws_region}d"
+}
+
+resource "aws_route_table_association" "public_RT_association" {
+  subnet_id = aws_subnet.project_subnet.id
+  route_table_id = aws_route_table.public_RT.id
 }
 
 resource "aws_security_group" "jenkins_security_group" {
