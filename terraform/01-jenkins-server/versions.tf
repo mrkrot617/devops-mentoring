9905c351e5b0d8f1e5ac1dc318ed8976dea55e09
Ivan Novitsky <mrkrot617@gmail.com>
2022-01-25 11:16:57 +0300

    Unlock and add terraform.lock.hcl to repository

--- terraform/01-jenkins-server/versions.tf
+++ terraform/01-jenkins-server/versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.73"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.1.0"
+    }
   }
 
   required_version = ">= 1.1.4"
