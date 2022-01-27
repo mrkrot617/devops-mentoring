7865aaa76aa69c8197b55b2860c7f43ccabc1eae
Ivan Novitsky <mrkrot617@gmail.com>
2022-01-27 11:39:56 +0300

    Allow http/s for Jenkins server

--- terraform/02-jenkins-server/main.tf
+++ terraform/02-jenkins-server/main.tf
@@ -30,6 +30,20 @@ resource "aws_security_group" "jenkins_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
